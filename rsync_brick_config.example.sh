05db5533a0a50091ecf735463b8c847c48a40acc
BuildTools <unconfigured@null.spigotmc.org>
2025-08-24 20:02:01 -0500

    updating example configs for brick, mini plus, and cubexx, examples for RP4pro and steamdeck are a little out of date, will have to revisit later.

--- rsync_brick_config.example.sh
+++ rsync_brick_config.example.sh
@@ -4,8 +4,181 @@
 target_dir="/your/target/path/here"
 
 # Add or adjust any system-specific excludes or renames as needed
+# If it's commented out, it will be synced
 exclude_dirs=(
-  # Add system directories to exclude here
+  # Media and assets
+  "screenshots"
+  "titlescreens"
+  #"videos"
+  #"box2dfront"
+  "bios"
+
+  # Arcade and emulation
+  #"mame2003plus"
+  #"mame2010"
+  "mame2003" 
+  "arcade"
+  "mame"
+  #"fbneo"
+  #"cps1"
+  #"cps2"
+  #"cps3"
+  #"naomi"
+  "naomi2"
+  #"atomiswave"
+
+  # Sega systems
+  #"sg1000"
+  #"mastersystem"
+  #"megadrive"
+  "segacd"
+  #"sega32x"
+  #"saturn"
+  "dreamcast"
+
+  #"gamegear"
+
+  # Nintendo systems
+  #"nes"
+  #"fds"
+  #"snes"
+  #"n64"
+  #"n64dd"
+  "gc"
+  "wii"
+  "wiiu"
+  "switch"
+
+  #"gameandwatch"
+  #"pokemini"
+  #"gb"
+  #"gbc"
+  #"virtualboy"
+  #"gba"
+  "ds"
+  "3ds"
+  #"sufami"
+
+  # PlayStation systems
+  #"psx"
+  "psp"
+  "ps2"
+  "ps3"
+
+  # Atari systems
+  #"atari2600"
+  #"atari5200"
+
+  #"atari7800"
+
+  #"jaguar"
+  "jaguarcd"
+  
+  #"lynx"
+
+  # other gen 1/2 consoles
+#   "channelf"
+#   "o2em" 
+#   "astrocde"
+#  "apfm1000"
+#  "vc4000"
+#   "intellivision"
+#   "colecovision"
+#   "advision"
+#   "vectrex"
+#   "crvision"
+#   "arcadia"
+
+  # other gen 3 consoles 
+#   "multivision"
+#   "videopacplus"
+#   "pv1000"
+#   "scv"
+  
+  # other gen 4 consoles
+  "cdtv"
+#   "pcengine"
+  "pcenginecd"
+  #"supergrafx"
+#   "neogeo"
+#  "cdi"
+#  "gx4000"
+  #"supracan"
+
+  # other gen 5 consoles
+ #"3do"
+ #"amigacd32"
+ #"pcfx"
+ "neogeocd"
+
+  # portable consoles
+#"gamate"
+#"gmaster"
+#  "supervision"
+#   "megaduck" 
+
+#  "gamecom"
+#   "ngpc"
+#   "ngp"
+#   "wswan"
+#   "wswanc"
+#  "gp32"
+
+  # Fantasy consoles/computers
+  #"arduboy"
+  #"lowresnx"
+  #"lutro"
+  "pico8"
+  #"tic80"
+  #"uzebox"
+  #"vircon32"
+  #"wasm4"
+  #"scummvm"
+  #"openbor"
+  "vpinball" #
+  #"zmachine"
+
+  # Home computers
+#   "pdp1" #
+#   "apple2"
+#   "pet" #
+  # "atari800"
+  # "atom" #
+  # "ti99"
+  # "c20"
+  # "coco"
+  # "dragon32"
+  # "pc88"
+  # "zx81"
+  # "bbc"
+  # "x1"
+  # "zxspectrum"
+  # "c64"
+  # "pc98" #
+  # "fm7"
+  # "tutor" #
+  # "electron" #
+  # "camplynx" #
+  # "msx1"
+  # "adam" #
+  # "spectravideo"
+  # "amstradcpc"
+  # "macintosh" #
+  # "thompson" #
+  # "cplus4"
+  # "laser310" #
+  # "oric" # Oric Atmos
+  # "atarist"
+  # "msx2"
+  # "c128"
+  # "apple2gs"
+  # "archimedes"
+  # "xegs" # Atari XE Game System
+  # "amiga500"
+  # "x68000"
+  # "fmtowns"
+  # "samcoupe"
+
 )
 
 declare -A rename_folders=(
--- rsync_miniplus_config.example.sh
+++ rsync_miniplus_config.example.sh
@@ -1,17 +1,187 @@
 # where your files will go
 target_dir="/your/target/path/here"
 
-# directories you do not want to copy
-exclude_dirs=("screenshots" "titlescreens" "videos" "box2dfront" "bios" "dreamcast" "psp" )
+# Add or adjust any system-specific excludes or renames as needed
+# If it is commented out, it will be synced
+exclude_dirs=(
+  # Media and assets
+  "screenshots"
+  "titlescreens"
+  #"videos"
+  #"box2dfront"
+  "bios"
 
-# Add your specific renames here. Example: ["snes"]="SFC"
+  # Arcade and emulation
+  #"mame2003plus"
+  "mame2010"
+  "mame2003" 
+  "arcade"
+  "mame"
+  "fbneo"
+  #"cps1"
+  #"cps2"
+  #"cps3"
+  "naomi"
+  "naomi2"
+  "atomiswave"
+
+  # Sega systems
+  #"sg1000"
+  #"mastersystem"
+  #"megadrive"
+  #"segacd"
+  #"sega32x"
+  "saturn"
+  "dreamcast"
+
+  #"gamegear"
+
+  # Nintendo systems
+  #"nes"
+  #"fds"
+  #"snes"
+  "n64"
+  "gc"
+  "wii"
+  "wiiu"
+  "switch"
+
+  #"gameandwatch"
+  #"pokemini"
+  #"gb"
+  #"gbc"
+  #"gba"
+  #"ds"
+  "3ds"
+  #"sufami"
+
+  # PlayStation systems
+  #"psx"
+  "psp"
+  "ps2"
+  "ps3"
+
+  # Atari systems
+  #"atari2600"
+  #"atari5200"
+
+  #"atari7800"
+
+  "jaguar" ##
+  "jaguarcd"
+  
+  #"lynx"
+
+  # other gen 1/2 consoles
+  #"channelf"
+  #"o2em" 
+  "astrocde"
+  #"intellivision"
+  #"colecovision"
+  "advision"
+  #"vectrex"
+  "crvision"
+  "arcadia"
+  "apfm1000"
+  "vc4000"
+
+  # other gen 3 consoles 
+  "multivision"
+  #"videopacplus"
+  "pv1000"
+  "scv"
+  
+  # other gen 4 consoles
+  "cdtv"
+  #"pcengine"
+  #"pcenginecd"
+  #"supergrafx"
+  #"neogeo"
+  "cdi"
+  "gx4000"
+  "supracan"
+
+  # other gen 5 consoles
+  "3do" ##
+  "amigacd32" ##
+  "pcfx" ##
+  "neogeocd"
+
+  # portable consoles
+  "gamate"
+  "gmaster"
+  #"supervision"
+  #"megaduck" 
+
+  "gamecom"
+  "ngp"
+  #"ngpc"
+  "wswan"
+  #"wswanc"
+ 
+  "gp32"
+
+  # Fantasy consoles/computers
+  #"arduboy"
+  "lowresnx" ##
+  "lutro" ##
+  "pico8"
+  #"tic80"
+  "uzebox" ##
+  "vircon32"
+  "wasm4"
+  #"scummvm"
+  "openbor" ##
+  "vpinball" #
+  "zmachine"
+
+  # Home computers
+  "pdp1" #
+  "apple2"
+  "pet" ##
+  "atari800" ##
+  "atom" #
+  "ti99"
+  #"c20"
+  "coco"
+  "dragon32"
+  "pc88" ##
+  "zx81" ##
+  "bbc"
+  "x1"
+  #"zxspectrum"
+  #"c64"
+  "pc98" ##
+  "fm7"
+  "tutor" #
+  "electron" #
+  "camplynx" #
+  #"msx1"
+  "adam" #
+  "spectravideo" 
+  #"amstradcpc"
+  "macintosh" #
+  "thompson" #
+  "cplus4"
+  "laser310" #
+  "oric" # Oric Atmos
+  "atarist" ##
+  #"msx2"
+  "c128"
+  "apple2gs"
+  "archimedes"
+  "xegs" # Atari XE Game System 
+  #"amiga500"
+  "x68000" ##
+  "fmtowns"
+  "samcoupe"
+)
+
 declare -A rename_folders=(
-  # ["original_system_name"]="new_rg35xxh_name"
-  # Example: ["snes"]="SFC"
   ["snes"]="SFC"
   ["nes"]="FC"
   ["megadrive"]="MD"
-  ["wonderswancolor"]="WS"
+  ["wswanc"]="WS"
   ["mastersystem"]="MS"
   ["atari800"]="EIGHTHUNDRED"
   ["atari2600"]="ATARI"
@@ -32,4 +202,7 @@ declare -A rename_folders=(
   ["videopacplus"]="VIDEOPAC"
   ["channelf"]="FAIRCHILD"
   ["tic80"]="TIC"
+  ["pico8"]="PICO"
+  ["ngpc"]="NGP"
+
 )
